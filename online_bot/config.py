06a89eccda5f3a64a3e95a6fdde3922dc0726821
nikitasuhomlin <nikitasuhomlin1995@gmail.com>
2022-07-20 18:55:30 +0300

    telegram bot 2pizza

---
+++ online_bot/config.py
@@ -0,0 +1,4 @@
+token = '5424018005:AAHxdANvTWwTCh9LMc3wWYX6H-FbmjO63HQ'
+YandexKassa_token = '381764678:TEST:39866'
+sber_token = '401643678:TEST:05c20aef-09c0-4fa2-b6c1-cc8393ae3a80'
+payMaster_token = '1744374395:TEST:352274d9e595cd734c94'
